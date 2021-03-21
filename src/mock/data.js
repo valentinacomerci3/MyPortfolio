02bc2cf4301a7566958e1821308e4d77a8d96a7a
valentina comerci <valentinacomerci3@gmail.com>
2021-03-21 05:58:51 -0700

    Portfolio first commit

--- src/mock/data.js
+++ src/mock/data.js
@@ -10,8 +10,8 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Valentina Comerci',
+  subtitle: 'Full stack Developer',
   cta: '',
 };
 

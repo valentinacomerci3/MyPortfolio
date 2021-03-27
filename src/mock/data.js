fc234a35477fefda2237a8f17604635049d86b01
valentina comerci <valentinacomerci3@gmail.com>
2021-03-27 10:04:05 -0700

    updates

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,47 +2,47 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Valentina Comerci', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
   name: 'Valentina Comerci',
-  subtitle: 'Full stack Developer',
+  subtitle: 'Aspiring Web Developer',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: 'HI, my name is Valentina. I grew up in Rome, Italy and discovered my passion for web development while living in San Francisco, California. ',
+  paragraphTwo: 'I know that I can provide a fresh take and an extraordinary approach to every project. I am always open to challenges and constructive feedback. ',
+  paragraphThree: 'I am open-minded and learn new things quickly. I want to become a team player and dedicate all my skills and talents to develop high-quality and unique products.',
+  resume: 'https://docs.google.com/document/d/1ZSHHTa_Zx602uUgCXTv3Du2wnzWaAFOv0ECQlk2tnjE/edit?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'todoapp.png',
+    title: 'Todo App',
+    info: ' A simple Todo-list built with React. ',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/valentinacomerci3/TodoList', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'Nucampsite.jpg',
+    title: 'NucampSite review Website',
+    info: 'built with React and Redux',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/valentinacomerci3/Reactapp', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
@@ -59,7 +59,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'valentinacomerci3@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,28 +67,23 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'codepen',
-      url: '',
+      url: 'https://codepen.io/valentinacomerci',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/valentina-comerci-24582417a/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/valentinacomerci3',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
